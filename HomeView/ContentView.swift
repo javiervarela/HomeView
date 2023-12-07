e161a9bcacd85b31ddb7914ee216e2b9ba67e1a8
Javier Angel Varela Cebey <gekito@gmail.com>
2023-12-08 00:38:00 +0100

    Initial Commit

---
+++ HomeView/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HomeView
+//
+//  Created by Javier Angel Varela Cebey on 8/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HomeView/HomeViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewApp.swift
+//  HomeView
+//
+//  Created by Javier Angel Varela Cebey on 8/12/23.
+//
+
+import SwiftUI
+
+@main
+struct HomeViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
